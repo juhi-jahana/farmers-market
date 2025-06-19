f3f9a95cad107efd74606417800eca331af83efb
juhi-jahana <juhi.digitally@gmail.com>
2025-06-19 08:52:05 +0530

    Update Mainlayout.jsx for layout fix

--- src/Page/Mainlayout.jsx
+++ src/Page/Mainlayout.jsx
@@ -3,14 +3,19 @@ import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import { Outlet } from 'react-router-dom';
 
-export const Mainlayout = () => {
+const Mainlayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      
+      {/* Content that pushes the footer down */}
       <main className="flex-grow pt-24 bg-white">
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
 };
+
+export default Mainlayout;

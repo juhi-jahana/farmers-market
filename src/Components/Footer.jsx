7c93c20c051fd67daffe2962c916dc9c0b24914f
juhi-jahana <juhi.digitally@gmail.com>
2025-06-19 10:48:23 +0530

    Update Footer.jsx for layout fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,9 +7,9 @@ import { FaRss } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <>
+    <div className="w-full">
+      {/* Top Footer Banner */}
       <div className="relative bg-[url('/footer_image.jpg')] bg-cover bg-center bg-no-repeat min-h-[600px] md:min-h-[700px]">
-        {/* Overlay */}
         <div className="absolute inset-0 bg-black/40 z-0"></div>
 
         <div className="relative z-10 flex flex-col justify-center items-start px-6 md:px-24 h-full py-16">
@@ -33,6 +33,7 @@ const Footer = () => {
         </div>
       </div>
 
+      {/* Bottom Bar */}
       <div className="bg-[#1A1A1A] text-gray-400 py-6">
         <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
           <p className="text-sm mb-4 md:mb-0">Powered by Digital Lime Green</p>
@@ -44,7 +45,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

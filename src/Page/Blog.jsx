7e928cb7aeecfb88089b780f973113336ecfb304
juhi-jahana <juhi.digitally@gmail.com>
2025-06-23 11:17:05 +0530

    Update Blog.jsx for layout fix

--- src/Page/Blog.jsx
+++ src/Page/Blog.jsx
@@ -14,7 +14,7 @@ const Blog = () => {
       content:
         "Welcome to Demo Websites. This is your first post. Edit or delete it, then start writing!",
     },
-     {
+    {
       id: 2,
       title: "Another Post with Everything In It",
       author: "James",
@@ -23,14 +23,14 @@ const Blog = () => {
       content:
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur quam augue, vehicula quis, tincidunt vel, varius vitae, nulla. Sed convallis orci. Duis libero orci, pretium a, convallis quis, pellentesque a, dolor. Curabitur vitae nisi non dolor vestibulum...",
     },
-     {
+    {
       id: 3,
       title: "An Ordered List Post",
       author: "James",
       date: "Apr 20, 2016",
       category: "Uncategorized",
       content:
-      " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
+        " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
     },
     {
       id: 4,
@@ -39,7 +39,7 @@ const Blog = () => {
       date: "Apr 20, 2016",
       category: "Uncategorized",
       content:
-      " Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed eleifend urna eu sapien. Quisque posuere nunc eu massa. Praesent bibendum lorem non leo. Morbi volutpat, urna eu fermentum rutrum, ligula lacus interdum mauris, ac pulvinar libero pede a enim. Etiam commodo...",
+        " Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed eleifend urna eu sapien. Quisque posuere nunc eu massa. Praesent bibendum lorem non leo. Morbi volutpat, urna eu fermentum rutrum, ligula lacus interdum mauris, ac pulvinar libero pede a enim. Etiam commodo...",
     },
     {
       id: 5,
@@ -50,32 +50,32 @@ const Blog = () => {
       content:
         "Welcome to Demo Websites. This is your first post. Edit or delete it, then start writing!",
     },
-      {
+    {
       id: 6,
       title: "An Ordered List Post",
       author: "James",
       date: "Apr 20, 2012",
       category: "Uncategorized",
       content:
-      " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
+        " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
     },
-       {
+    {
       id: 7,
       title: "A Post With a Left-Aligned Image",
       author: "James",
       date: "Apr 20, 2009",
       category: "Uncategorized",
       content:
-      " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
+        " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
     },
-     {
+    {
       id: 8,
       title: "Quotes Time!",
       author: "James",
       date: "Apr 20, 2010",
       category: "Uncategorized",
       content:
-      " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
+        " Nulla sagittis convallis arcu. Sed sed nunc. Curabitur consequat. Quisque metus enim, venenatis fermentum, mollis in, porta et, nibh. Duis vulputate elit in elit. Mauris dictum libero id justo. Fusce in est. Sed nec diam. Pellentesque habitant morbi tristique senectus...",
     },
     {
       id: 9,
@@ -88,7 +88,7 @@ const Blog = () => {
     },
 
   ];
-   const oldPosts = [
+  const oldPosts = [
     {
       id: 10,
       title: "Old Post 1",
@@ -105,7 +105,7 @@ const Blog = () => {
       content: "Vestibulum in mauris semper tortor interdum ultrices.",
     },
   ];
-    const toggleCard = (id) => {
+  const toggleCard = (id) => {
     setExpandedCards((prev) => ({
       ...prev,
       [id]: !prev[id],
@@ -115,9 +115,9 @@ const Blog = () => {
 
   const postsToShow = showOld ? oldPosts : newPosts;
   const [isPlaying, setIsPlaying] = useState(false);
-    const scrollToTop = () => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    };
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   return (
     <>
@@ -129,58 +129,52 @@ const Blog = () => {
         </div>
       </div>
 
-{/* entries... */}
-<div>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-20 p-2  lg:px-40 lg:py-32 bg-[#F7F7F7]">
-     {postsToShow.map((post) => {
-        const isExpanded = expandedCards[post.id];
-        const isLong = post.content.length > 150;
+      {/* entries... */}
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-20 p-2  lg:px-40 lg:py-32 bg-[#F7F7F7]">
+          {postsToShow.map((post) => {
+            const isExpanded = expandedCards[post.id];
+            const isLong = post.content.length > 150;
 
-        const cardHeight = isExpanded
-          ? "h-auto"
-          : isLong
-          ? "h-[280px]"
-          : "h-[180px]";
+            const cardHeight = isExpanded
+              ? "h-auto"
+              : isLong
+                ? "h-[280px]"
+                : "h-[180px]";
 
-        return (
-          <div
-            key={post.id}
-            className={`border px-5 py-5 lg:px-4 lg:py-6 border-gray-400 bg-white shadow-md relative  transition-all duration-300 ${cardHeight}`}
-          >
-            <Link to={`/post/${post.id}`}>
-              <h2 className="text-xl font-normal text-black">{post.title}</h2>
-            </Link>
-            <p className="text-sm font-medium text-gray-500">
-              by {post.author} | {post.date}
-            </p>
-            <p className="mt-2 text-[17px] text-gray-600">{post.content}</p>
-
-            {isLong && (
-              <button
-                onClick={() => toggleCard(post.id)}
-                className="absolute bottom-2 right-4 text-blue-600 text-sm"
+            return (
+              <div
+                key={post.id}
+                className={`border px-5 py-5 lg:px-4 lg:py-6 border-gray-400 bg-white shadow-md relative  transition-all duration-300 ${cardHeight}`}
               >
-              </button>
-            )}
+                <Link to={`/post/${post.id}`}>
+                  <h2 className="text-xl font-normal text-black">{post.title}</h2>
+                </Link>
+                <p className="text-sm font-medium text-gray-500">
+                  by {post.author} | {post.date}
+                </p>
+                <p className="mt-2 text-[17px] text-gray-600">{post.content}</p>
+
+                {isLong && (
+                  <button
+                    onClick={() => toggleCard(post.id)}
+                    className="absolute bottom-2 right-4 text-blue-600 text-sm"
+                  >
+                  </button>
+                )}
+              </div>
+            );
+          })}
+          <div className="mt-10 text-center">
+            <button
+              onClick={() => setShowOld(!showOld)}
+              className="bg-text text-[#02D002] px-6 py-2 rounded"
+            >
+              {showOld ? "← New Entries" : "Old Entries →"}
+            </button>
           </div>
-        );
-      })}
-       <div className="mt-10 text-center">
-  <button
-    onClick={() => setShowOld(!showOld)}
-    className="bg-text text-[#02D002] px-6 py-2 rounded"
-  >
-    {showOld ? "← New Entries" : "Old Entries →"}
-  </button>
-</div>
-    </div>
-    </div>
-
-
-
-
-
-
+        </div>
+      </div>
       {/* form... */}
       <div className="bg-gray-50 py-16 px-6 md:px-12 lg:px-52 lg:py-40">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 items-center gap-12">
